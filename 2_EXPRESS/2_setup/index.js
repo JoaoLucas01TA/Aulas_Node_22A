c89835a318d8341480fa32fe4ae9d8238fe84756
Juca <joaofslucas2022@gmail.com>
2023-08-31 15:14:41 -0300

    Setup do Express

---
+++ 2_EXPRESS/2_setup/index.js
@@ -0,0 +1,10 @@
+// Importar o Express
+
+const express = require('express')
+const app = express()
+
+// GET e POST
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Estou utilizando o Express!")
+})
